b2941c99fe941baa0f5100373ceb102d0db3b0d1
SDS-012 <sds-012@70-12-246-246.pools.spcsdns.net>
2017-04-25 17:37:02 +0900

    add home

--- FirstSample/ViewController.swift
+++ FirstSample/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func unwind(_ segue: UIStoryboardSegue) { }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
